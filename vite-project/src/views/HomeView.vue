a518899562b7366b8f83765c9622e6b67ca513d3
Nosha08 <noah.rozin@outlook.com>
2024-02-15 11:36:01 -0500

    a

--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -16,24 +16,27 @@ async function fetchData() {
       headers: {
         'X-Api-Key': key
       }
-    });
+    })
     location.value = ''
 
     if (response.ok) {
       const data = await response.json()
       console.log(data[0])
 
-      bubbles.push({
-        latitude: data[0].latitude,
-        longitude: data[0].longitude,
-        radius: data[0].population/1500000,
-        fillKey: '#306596'
-      })
-
       const cityPop = data[0].population
       const percentage = cityPop/globalPop * 100
       total.value += percentage
       population.value += cityPop
+
+      bubbles.push({
+        latitude: data[0].latitude,
+        longitude: data[0].longitude,
+        radius: Math.sqrt(data[0].population)/750,
+        fillKey: 'bubble',
+        city: data[0].name,
+        population: data[0].population,
+        percentage: percentage
+      })
 
     } else {
       console.error('Error:', response.status)
@@ -54,16 +57,34 @@ new Datamap({
   geographyConfig: {
     highlightOnHover: false,
     popupOnHover: false
+  },
+  fills: {
+    'defaultFill': '#ADDFFF',
+    'bubble': '#FA8072'
   }
 })
+
+const zoom = d3.behavior.zoom()
+  .scaleExtent([1, 100])
+  .on('zoom', function() {
+    map.svg.selectAll('g').attr('transform', 'translate(' + d3.event.translate + ')scale(' + d3.event.scale + ')')
+  })
+
+map.svg.call(zoom)
 
 
 function create() {
   console.log(bubbles)
 
-
   setTimeout(() => { 
-      map.bubbles(bubbles)
+      map.bubbles(bubbles, {
+        popupTemplate: function(geo, data) {
+            return '<div class="hover">' +
+                   'City: ' + data.city +
+                   '<br/>Population: ' + data.population.toLocaleString('en-US') +
+                   '<br/>Percentage: ' + data.percentage.toFixed(3) + ('%') +
+                   '</div>';
+        }})
   }, 1000)
 }
 </script>
@@ -74,13 +95,18 @@ function create() {
     <form @submit.prevent="fetchData()">
       <input v-model="location" type="text" id="name" placeholder="Input Location Name"/>
     </form>
-    <h1>You have located {{ population.toLocaleString('en-US') }} or a total of {{ total.toFixed(2) }}% of the global population!</h1>
+    <h1>You have located {{ population.toLocaleString('en-US') }} or a total of {{ total.toFixed(3) }}% of the global population!</h1>
   </main>
 </template>
 
-<style scoped>
+<style>
 main {
   text-align: center;
   margin-top: 4rem;
+}
+
+.datamaps-hoverover {
+  margin-top: 10rem;
+  right: 100px;
 }
 </style>
