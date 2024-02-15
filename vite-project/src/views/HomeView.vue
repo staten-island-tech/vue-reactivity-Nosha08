03010f466fc8be3dd1549909c24cfe4809008bb9
Nosha08 <noah.rozin@outlook.com>
2024-02-15 01:35:21 -0500

    a

--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -4,7 +4,8 @@ import { ref } from 'vue'
 
 const location = ref('')
 const globalPop = 8091031090
-let total = 0
+let total = ref(0)
+let population = ref(0)
 
 async function fetchData() {
   const key = 'Kx1r7YwFeSKRRUgRBUs4Ng==1l7XCqxVaMONZ1fv'
@@ -20,10 +21,20 @@ async function fetchData() {
 
     if (response.ok) {
       const data = await response.json()
+      console.log(data[0])
+
+      bubbles.push({
+        latitude: data[0].latitude,
+        longitude: data[0].longitude,
+        radius: data[0].population/1500000,
+        fillKey: '#306596'
+      })
+
       const cityPop = data[0].population
       const percentage = cityPop/globalPop * 100
-      total += percentage
-      console.log(total + '%')
+      total.value += percentage
+      population.value += cityPop
+
     } else {
       console.error('Error:', response.status)
       return
@@ -33,8 +44,28 @@ async function fetchData() {
     console.error('Request failed:', error)
   }
 
+  create()
 }
 
+let bubbles = []
+const map = 
+new Datamap({
+  element: document.getElementById('container'),
+  geographyConfig: {
+    highlightOnHover: false,
+    popupOnHover: false
+  }
+})
+
+
+function create() {
+  console.log(bubbles)
+
+
+  setTimeout(() => { 
+      map.bubbles(bubbles)
+  }, 1000)
+}
 </script>
 
 <template>
@@ -43,5 +74,13 @@ async function fetchData() {
     <form @submit.prevent="fetchData()">
       <input v-model="location" type="text" id="name" placeholder="Input Location Name"/>
     </form>
+    <h1>You have located {{ population.toLocaleString('en-US') }} or a total of {{ total.toFixed(2) }}% of the global population!</h1>
   </main>
 </template>
+
+<style scoped>
+main {
+  text-align: center;
+  margin-top: 4rem;
+}
+</style>
